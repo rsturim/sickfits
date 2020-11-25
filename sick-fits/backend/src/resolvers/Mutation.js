e648e65c1def67a102d217c8b953838cb670486b
Rich Sturim <rich.sturim@coxautoinc.com>
2020-11-25 17:58:03 -0500

    getting first mutations and queries wired up

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -1,3 +1,19 @@
-const Mutations = {};
+const { Prisma } = require("prisma-binding");
+
+const Mutations = {
+    async createItem(parent, args, ctx, info) {
+        const item = await ctx.db.mutation.createItem(
+            {
+                data: {
+                    ...args,
+                },
+            },
+            info,
+        );
+        console.log("item: ", item);
+        console.log("------------------------------------------");
+        return item;
+    },
+};
 
 module.exports = Mutations;
